fde42ab76c9c1993ef1d01c0ce9cb4a30b455bd5
Maxime Bel <maxime.bel@epitech.eu>
2018-05-24 16:31:22 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# noHandlerLyraWebPayment
-parce que j'ai pas envie de tout recommencer
+# lyra-web-payment
+
+Demonstrator Webauthn + Payment request + Payment handler
+
+## Requirement
+
+The website should be tested with chrome canary with "Service Worker payment apps" and "Web Authentication API" flags enabled.
+
+## Test it
+
+Currently online [here](https://test-payment-handler.appspot.com/).
+
+### How to test
+
+To test it, you have to install the Payment handler at the bottom of the page.
+
+After that, click the button Lauch Payment Request next to the phone.
+
+If the installed payment handler does not appear, uninstall and reinstall it.
+
+If it appears, select it and click the paiment button, it will probably fail the first time, just reclick on Launch Payment Request button and retry.
+
+A login page will appear. It does not works at this time.
+
+If you want to try the webauthn authentication, you can do it [here](https://test-payment-handler.appspot.com/pages/authentication.html).
+
+This project uses as basis:
+
+     https://github.com/fido-alliance/webauthn-demo for the webauthn part.
+     https://github.com/madmath/payment-request-show/tree/master/bobpay for the payment handler part.
+     https://github.com/rsolomakhin/rsolomakhin.github.io/tree/master/pr/apps/basic-card for the payment request part.
---
+++ static/js/webauthn.auth.js
@@ -0,0 +1,148 @@
+'use strict';
+
+let getMakeCredentialsChallenge = (formBody) => {
+    return fetch('/webauthn/register', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formBody)
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            if (response.status !== 'ok')
+                throw new Error(`Server responed with error. The message is: ${response.message}`);
+
+            return response
+        })
+}
+
+let sendWebAuthnResponse = (body) => {
+    return fetch('/webauthn/response', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response)
+            if (response.status !== 'ok')
+                throw new Error(`Server responed with error. The message is: ${response.message}`);
+
+            return response
+        })
+}
+
+//
+// Username is credit card number and password is date (testing phase)
+//
+
+$('#card-form').submit(function (event) {
+    event.preventDefault();
+
+    let cc_number = this.number.value;
+    let expDate = this.expDate.value;
+    let name = "testFakeHolder";
+
+    // let username = this.username.value;
+    // let password = this.password.value;
+
+    if (!cc_number || !name || !expDate) {
+        console.log('Name , cc_number or expDate is missing!')
+        return
+    }
+
+    getMakeCredentialsChallenge({ cc_number, name, expDate })
+        .then((response) => {
+            let publicKey = preformatMakeCredReq(response);
+            return navigator.credentials.create({ publicKey })
+        })
+        .then((response) => {
+            let makeCredResponse = publicKeyCredentialToJSON(response);
+            console.log(makeCredResponse)
+            makeCredResponse.isLoginAuthType = false;
+            return sendWebAuthnResponse(makeCredResponse)
+        })
+        .then((response) => {
+            if (response.status === 'ok') {
+                console.log("success")
+                alert("registration successful !")
+                window.location.replace("http://localhost:8080")
+            } else {
+                console.log(`Server responed with error. The message is: ${response.message}`);
+            }
+        })
+        .catch((error) => console.log(error))
+})
+
+let getGetAssertionChallenge = (formBody) => {
+    console.log(formBody);
+    return fetch('/webauthn/login', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formBody)
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            if (response.status !== 'ok')
+                throw new Error(`Server responed with error. The message is: ${response.message}`);
+
+            return response
+        })
+}
+
+//
+//   Username is cc-number and password is exp date (testing)
+//
+
+$('#payment-form').submit(function (event) {
+    event.preventDefault();
+
+    // let username = this.username.value;
+    // let password = this.password.value;
+
+    let cvv = this.cvv.value;
+    let cc_number = this.number.value;
+    let expDate = this.expDate.value;
+
+    if (!cvv || !cc_number || !expDate) {
+        console.log("Missing cc-number or cvv or expiration date");
+    }
+
+    console.log(cvv);
+    console.log(cc_number);
+    console.log(expDate);
+
+    getGetAssertionChallenge({ cc_number, expDate, cvv })
+        .then((response) => {
+            console.log(response)
+            let publicKey = preformatGetAssertReq(response);
+            return navigator.credentials.get({ publicKey })
+        })
+        .then((response) => {
+            // easiest solution found to make double factor auth and not 2 steps auth
+            let getAssertionResponse = publicKeyCredentialToJSON(response);
+            getAssertionResponse.usernameToTest = cc_number;
+            getAssertionResponse.passwordToTest = expDate;
+            getAssertionResponse.isLoginAuthType = true;
+            return sendWebAuthnResponse(getAssertionResponse)
+        })
+        .then((response) => {
+            if (response.status === 'ok') {
+                // ajouter le back vers la page d'acceuil
+                console.log("success")
+                alert("payment successful !")
+                window.location.replace("http://localhost:8080")
+            } else {
+                console.log(`Server responed with error. The message is: ${response.message}`);
+            }
+        })
+        .catch((error) => console.log(error))
+})
